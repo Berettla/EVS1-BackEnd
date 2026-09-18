085c03fc6597aa6e809ceaf3b5d16624743ccb61
Berettla <agunarci8@gmail.com>
2026-09-17 23:36:54 -0300

    Agrega página inicio

--- Evs1/urls.py
+++ Evs1/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from . import views
 
 urlpatterns = [
+    path('', views.inicio, name='inicio'),
     path('admin/', admin.site.urls),
     path('musica/', include('musica.urls')),
     path('juegos/', include('juegos.urls')),
